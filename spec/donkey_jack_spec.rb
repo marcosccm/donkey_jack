fb87c5cbe147a334e5a7918a5ef655a9d9972ab4
Marcos Matos <marcosccm@gmail.com>
2013-12-09 23:33:29 -0200

    Promise can be rejected

--- spec/donkey_jack_spec.rb
+++ spec/donkey_jack_spec.rb
@@ -9,13 +9,27 @@ class Promise
     Promise.new(:fulfilled)
   end
 
+  def reject
+    Promise.new(:rejected)
+  end
+
   def fulfilled?
     @state == :fulfilled
+  end
+
+  def rejected?
+    @state == :rejected
   end
 end
 
 describe "A Promise" do
   context "when pending" do
+    it "can be rejected" do
+      p = Promise.new
+      rejected = p.reject
+      expect(rejected).to be_rejected
+    end
+
     it "can be fulfilled" do
       p = Promise.new
       fulfilled = p.fulfill
