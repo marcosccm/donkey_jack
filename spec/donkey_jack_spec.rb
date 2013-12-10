990ea00a2fe3c7a3b032163e57e41b2757d46608
Marcos Matos <marcosccm@gmail.com>
2013-12-09 23:31:31 -0200

    Promises can be fulfilled

--- spec/donkey_jack_spec.rb
+++ spec/donkey_jack_spec.rb
@@ -1,8 +1,25 @@
 require "rspec"
 
-describe "A Promisse" do
+class Promise
+  def initialize(state=:pending)
+    @state = state
+  end
+
+  def fulfill
+    Promise.new(:fulfilled)
+  end
+
+  def fulfilled?
+    @state == :fulfilled
+  end
+end
+
+describe "A Promise" do
   context "when pending" do
     it "can be fulfilled" do
+      p = Promise.new
+      fulfilled = p.fulfill
+      expect(fulfilled).to be_fulfilled
     end
   end
 end
